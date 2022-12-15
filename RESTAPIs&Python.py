e65e900138d5b4b00d90af5205a5efc7114b25e7
Jonathan Cavalcante <jonathancmpc@gmail.com>
2022-12-15 17:26:03 -0300

    create_functions

--- RESTAPIs&Python.py
+++ RESTAPIs&Python.py
@@ -2,13 +2,24 @@ import requests
 import ApiKey
 
 api_key = ApiKey.api_key
-url = f'https://newsapi.org/v2/everything?qInTitle=stock%20market&from=2022-11-15&to=2022-11-15&sortBy=popularity&language=en&apiKey={api_key}'
-r = requests.get(url)
-content = r.json()
 
-articles = content['articles']
-#print(content['articles'][1]['source']['name'])
-print(type(articles))
+def get_news(topic,from_date,to_date,language='en',api_key_=api_key):
+  url = f'https://newsapi.org/v2/everything?qInTitle={topic}&from={from_date}&to={to_date}&sortBy=popularity&language={language}&apiKey={api_key_}'
+  r = requests.get(url)
+  content = r.json()
 
-for article in articles:
-    print('TITLE\n', article['title'], '\nDESCRIPTION\n', article['description'])
+  articles = content['articles']
+  #print(content['articles'][1]['source']['name'])
+
+  results = []
+
+  for article in articles:
+    results.append(f"TITLE\n' {article['title']}, '\nDESCRIPTION\n', {article['description']}")
+
+  return results
+
+
+def main():
+  print(get_news(topic='united states', from_date='2022-12-14', to_date='2022-12-15'))
+
+main()
